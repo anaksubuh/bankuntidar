e841a67b545e3c5045f44726480c07ff2229cef3
anak subuh <75495151+anaksubuh@users.noreply.github.com>
2024-09-24 23:42:13 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Judul aplikasi
+st.title("Gambar Lucu")
+
+# Menampilkan gambar
+st.image("https://i.pinimg.com/originals/ab/5c/0e/ab5c0e146693b8e456b24e6ef6b80dc3.jpg", caption="Kucing lucu", use_column_width=True)
+
+# Menambahkan teks lucu
+st.write("Terkadang kita semua butuh sedikit keceriaan! 😄")
